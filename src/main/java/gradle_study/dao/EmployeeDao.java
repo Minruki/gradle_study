7199b951d09c11e2ad8cf4ffc4b3d267a40d645b
Min <rukiminangel@naver.com>
2021-02-26 11:14:52 +0900

    native_jdbc_programming 이전 작업완료

---
+++ src/main/java/gradle_study/dao/EmployeeDao.java
@@ -0,0 +1,19 @@
+package gradle_study.dao;
+
+import java.util.List;
+
+import gradle_study.dto.Employee;
+
+public interface EmployeeDao {
+	
+	List<Employee> selectEmployeeByAll();
+	Employee selectEmployeeByNo(Employee emp);
+	
+	int insertEmployee(Employee employee);	
+	int updateEmployee(Employee employee);	
+	int deleteEmployee(Employee employee);
+	
+
+
+
+}
